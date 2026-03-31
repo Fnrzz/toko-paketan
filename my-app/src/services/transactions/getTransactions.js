1aaa3796c0f2f95d20048ee9cc053364709298da
Fnrzz <faridnurrganteng123@gmail.com>
2026-03-31 21:58:23 +0700

    [PUSH] delete console log

--- my-app/src/services/transactions/getTransactions.js
+++ my-app/src/services/transactions/getTransactions.js
@@ -1,11 +1,8 @@
 export const getTransactions = async (userId) => {
-  console.log(userId);
   const result = await fetch(
     `http://localhost:3001/transactions?userId:eq=${userId}&_embed=product`,
   );
   const transactions = await result.json();
-
-  console.log(transactions);
 
   if (transactions.length === 0) {
     return { success: false, error: "Transactions not found" };
